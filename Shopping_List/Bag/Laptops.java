afceafca7a568f34b16ff41467db6d5a491adc9a
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2020-07-16 20:37:40 +0800

    购物车程序（中作业）

---
+++ Shopping_List/Bag/Laptops.java
@@ -0,0 +1,22 @@
+package Bag;
+import Bag.Goods;
+public class Laptops extends Goods {
+    private String CPU;
+    private String ScreenSize;
+
+    public void Set_CPU(String temp6) {
+        CPU = temp6;
+    }
+
+    public void Set_ScreenSize(String temp7) {
+        ScreenSize = temp7;
+    }
+
+    public String Get_CPU() {
+        return CPU;
+    }
+
+    public String Get_ScreenSize() {
+        return ScreenSize;
+    }
+}
